d5603dd98c65386473d6e91a4c75b23770b8d8b7
SwoopCodes <pat2graszk@gmail.com>
2024-12-16 22:03:45 +0000

    Added extra JS code and tables
    Added code to place random letters into table
    
    Added onclick listener to every table cell

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-const word = "HELLO";
 
-let xCoordinate = 0;
-let yCoordinate = 0;
+const word = "HELLO"; // declares word that needs to be found
 
-let i = 0;
+let selected = "" // selected letters by user
 
+const placeholders = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // letters of the alphabet to place randomly across the board
+
+
+let xCoordinate = 0; // x coordinates of table
+let yCoordinate = 0; // y coordinates of table
+
+const tableCells = document.querySelectorAll("td"); // finds all TD elemends
+
+// places random letter into every cell
+tableCells.forEach(cell => {
+    let randomNum = Math.floor(Math.random() * 26) // generates random number between 0 and 26
+    cell.textContent = placeholders[randomNum] // places letters into table cell
+});
+
+
+let i = 0; // i 
+
+// places word into table
+// to be changed later
 for(letter in word){
-    let places = document.getElementById(`x${xCoordinate}y${yCoordinate}`);
-    places.textContent = word[i];
-    x++
-    i++
+    let places = document.getElementById(`x${xCoordinate}y${yCoordinate}`); // gets x and y coordiantes of table
+    places.textContent = word[i]; // places each letter based on i variable
+    xCoordinate++; // increases x coordinate
+    i++;
 }
+
+//adds event listener to every cell
+// on click - append cell data into 'selected' variable
+tableCells.forEach(cell => {
+    cell.addEventListener("click", function(){
+        selected += cell.textContent
+    })
+});
